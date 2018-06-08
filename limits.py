63aae7fee6191772f90ccc6ec20d7249502e12a7
i-shir <40035961+i-shir@users.noreply.github.com>
2018-06-08 10:07:59 -0400

    Update limits.py

--- limits.py
+++ limits.py
@@ -1,13 +1,24 @@
+import math
 #Iroha and Emily
 #Limit
 #y=x^2
 
 
 a=float(input('Place to evaluate limit: '))
-h=float(input('Give h value:'))
+tol = float(input('tolerance: '))
+h=0.0000001
 m=a-h
-print('m:') 
-print(m)
+print('m:',m) 
 p=a+h
-print('p:') 
-print(p)
+print('p:',p) 
+
+M=m**2
+P=p**2
+
+d=P-M
+print ('d:',d)
+
+if (abs(float(d)) <= (tol)):
+    print('limit exists')
+else:
+    print('limit does not exist')
